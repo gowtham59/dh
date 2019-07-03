0470ba227db9ef44c37fa96543f9cdf713c44015
gowtham59 <47033355+gowtham59@users.noreply.github.com>
2019-07-03 15:45:52 +0530

    Update d.py

--- d.py
+++ d.py
@@ -1,13 +1,8 @@
-vj1,vs2=map(int,input().split())
-v3=[]
-s4=[]
-a5=[int(vj1) for vj1 in input().split() ]
-for i in range(0,vs2):
-    u,v=map(int,input().split())
-    for j in range(u-1 ,v):
-        s4.append(a[j])
-    x=sorted(s4)
-    v3.append(min(s4))
-    del s4[:]
-for sz in range(0,len(v3)):
-    print(v3[sz])
+a1,b2=list(map(int,input().split()))
+c3=list(map(int,input().split()))[:a1]
+d4=list(map(int,input().split()))[:b2]
+e5=list(map(int,input().split()))[:b2]
+f6=list(map(int,input().split()))[:b2]
+print(min(c3+d4))
+print(min(c3+e5))
+print(min(c3+f6))
